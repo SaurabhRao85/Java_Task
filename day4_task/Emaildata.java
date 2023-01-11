1334cb95491e6bc286aef935673e8af58f863c5e
Saurabh Rao <raosaurabh85@gmail.com>
2023-01-11 21:36:16 +0530

    day4_task

---
+++ day4_task/Emaildata.java
@@ -0,0 +1,19 @@
+package Emaildata;
+
+public class Emaildata {
+
+    private String Username = "Saurabh1234";
+    private String password = "Saurabh@1234";
+
+    String getUsername() {
+        return this.Username;
+    }
+
+    String getpassword() {
+        return this.password;
+    }
+
+    void setpassword(String newpassword) {
+        this.password = newpassword;
+    }
+}
