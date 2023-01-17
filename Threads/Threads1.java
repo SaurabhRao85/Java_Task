b8a0a69b67cdbf5ae23b197027d0bb88d84dcf37
Saurabh Rao <raosaurabh85@gmail.com>
2023-01-17 09:09:05 +0530

    Threads

---
+++ Threads/Threads1.java
@@ -0,0 +1,23 @@
+package Threads;
+
+import java.lang.Thread;
+
+
+public class Threads1 extends Thread {
+
+    public static void main(String argvs[]) {
+
+        try {
+            for (int j = 0; j < 100; j++) {
+
+                Thread.sleep(1000);
+
+                // displaying the value of the variable
+                System.out.println(j);
+            }
+        } catch (Exception expn) {
+            // catching the exception
+            System.out.println(expn);
+        }
+    }
+}
